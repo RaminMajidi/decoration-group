5131bc2997dc474f374f16001b9b2cc0410aad40
RaminMajidi <raminmajididev01@gmail.com>
2023-07-25 21:08:33 +0330

    insert images and svg for navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+       //event fro sibar handler
+       var elems = document.querySelectorAll('.sidenav');
+       var instances = M.Sidenav.init(elems, {
+           edge: "right"
+       });
+       //********************* */
 });
