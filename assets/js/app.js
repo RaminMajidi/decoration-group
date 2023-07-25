95eeab4e07ae28685920abd8bbc717e3a12aa182
RaminMajidi <raminmajididev01@gmail.com>
2023-07-25 22:13:48 +0330

    creating Carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-       //event fro sibar handler
-       var elems = document.querySelectorAll('.sidenav');
-       var instances = M.Sidenav.init(elems, {
-           edge: "right"
-       });
-       //********************* */
+    //event fro sibar handler
+    var sideNav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sideNav, {
+        edge: "right"
+    });
+    //********************* */
+
+    //event fro top_carousel handler
+    var carousels = document.querySelectorAll('.carousel');
+    M.Carousel.init(carousels, {
+        fullWidth: true,
+        indicators: true,
+    });
+
+    setInterval(()=>{
+        M.Carousel.getInstance(carousels[0]).next()
+    },4000)
+    //********************* */
 });
