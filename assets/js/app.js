c580a353f171f8d21ab7d091a252beb0edf6163e
RaminMajidi <raminmajididev01@gmail.com>
2023-07-28 23:20:20 +0330

    meta tags for seo

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,14 +13,14 @@ document.addEventListener('DOMContentLoaded', function () {
     //********************* */
 
     //event for top_carousel handler
-    var carousels = document.querySelectorAll('.carousel');
-    M.Carousel.init(carousels, {
+    var carouselElems = document.querySelectorAll('.carousel.top_carousel');
+    M.Carousel.init(carouselElems, {
         fullWidth: true,
         indicators: true,
     });
 
     setInterval(() => {
-        M.Carousel.getInstance(carousels[0]).next()
+        M.Carousel.getInstance(carouselElems[0]).next()
     }, 5000)
     //********************* */
 
@@ -31,11 +31,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     //event  toast message for confirm comment in article page
-    document.getElementById('confirm_comment').addEventListener('click', function () {
+    document.getElementById('confirm_comment')?.addEventListener('click', function () {
         M.toast({
             html: 'نظر با موفقیت ثبت شد !',
             classes: 'green lighten-1'
         })
+    })
+    //********************* */
+
+
+    //event for gallery carousel handler
+    var galleryItems = document.querySelectorAll('.carousel.gallery_div');
+    M.Carousel.init(galleryItems, {});
+
+    var simpleItem = document.querySelectorAll('.simple_item');
+    for (const item of simpleItem) {
+        item.addEventListener('click', function () {
+            document.getElementById('gallery_section').classList.add('active');
+        })
+    }
+
+    document.getElementById('close_gallery_section').addEventListener('click', function () {
+        document.getElementById('gallery_section').classList.remove('active');
     })
     //********************* */
 
