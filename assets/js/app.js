b5c474b5a80a87a7572f87090827355179e2a34a
RaminMajidi <raminmajididev01@gmail.com>
2023-07-29 14:50:21 +0330

    Change address files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     //event for sibar handler
-    var sideNav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sideNav, {
-        edge: "right"
-    });
+    if (document.querySelectorAll('.sidenav')) {
+        var sideNav = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(sideNav, {
+            edge: "right"
+        });
+    }
     //********************* */
 
     //event for fixed action btn handler
-    var elemsFloatingButton = document.querySelectorAll('.fixed-action-btn');
-    M.FloatingActionButton.init(elemsFloatingButton, {});
+    if (document.querySelectorAll('.fixed-action-btn')) {
+        var elemsFloatingButton = document.querySelectorAll('.fixed-action-btn');
+        M.FloatingActionButton.init(elemsFloatingButton, {});
+    }
     //********************* */
 
     //event for top_carousel handler
-    var carouselElems = document.querySelectorAll('.carousel.top_carousel');
-    M.Carousel.init(carouselElems, {
-        fullWidth: true,
-        indicators: true,
-    });
+    if (document.querySelector('.carousel.top_carousel')) {
+        var carouselElems = document.querySelectorAll('.carousel.top_carousel');
+        M.Carousel.init(carouselElems, {
+            fullWidth: true,
+            indicators: true,
+        });
 
-    setInterval(() => {
-        M.Carousel.getInstance(carouselElems[0]).next()
-    }, 5000)
+        setInterval(() => {
+            M.Carousel.getInstance(carouselElems[0]).next()
+        }, 5000)
+    }
     //********************* */
 
     //event for select input
-    var elems = document.querySelectorAll('select');
-    M.FormSelect.init(elems, {});
+    if (document.querySelectorAll('select')) {
+        var selectElems = document.querySelectorAll('select');
+        M.FormSelect.init(selectElems, {});
+    }
     //********************* */
+
+
 
 
     //event  toast message for confirm comment in article page
@@ -41,19 +51,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     //event for gallery carousel handler
-    var galleryItems = document.querySelectorAll('.carousel.gallery_div');
-    M.Carousel.init(galleryItems, {});
+    if (document.querySelectorAll('.carousel.gallery_div')) {
+        var galleryItems = document.querySelectorAll('.carousel.gallery_div');
+        M.Carousel.init(galleryItems, {});
 
-    var simpleItem = document.querySelectorAll('.simple_item');
-    for (const item of simpleItem) {
-        item.addEventListener('click', function () {
-            document.getElementById('gallery_section').classList.add('active');
+        var simpleItem = document.querySelectorAll('.simple_item');
+        for (const item of simpleItem) {
+            item.addEventListener('click', function () {
+                document.getElementById('gallery_section').classList.add('active');
+            })
+        }
+
+        document.getElementById('close_gallery_section')?.addEventListener('click', function () {
+            document.getElementById('gallery_section').classList.remove('active');
         })
     }
-
-    document.getElementById('close_gallery_section').addEventListener('click', function () {
-        document.getElementById('gallery_section').classList.remove('active');
-    })
     //********************* */
 
 });
